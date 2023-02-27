fd4d59bd84e7941a0ce62845dd5b2f837baa99b8
Mark Mac <markmac.web@gmail.com>
2023-02-27 13:48:33 +0000

    moved individual questions to single component

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,35 +1,33 @@
 import React, {useState, useEffect} from "react"
+import { nanoid } from "nanoid"
+import OneQuestion from "./OneQuestion"
+import SingleQuestion from "./OneQuestion"
 
 export default function Questions(){
     const [quizQuestions, setQuizQuestions] = useState([])
-  
+    
+
+    //fetch quiz questions
     useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&category=9&type=multiple")
       .then(res => res.json())
       .then(data => setQuizQuestions(data.results))
       }, [])
-  console.log(quizQuestions)
-   const questions = quizQuestions.map(e => {
-    const answers = e.incorrect_answers
-    if(answers.length < 4){
-        answers.push(e.correct_answer)
+  
+    function answerSelected(event){
+        console.log(event)
     }
-    
-    const answerElements = answers.map(answer => {
-        return(
-            <span dangerouslySetInnerHTML={{__html: answer}} id="answers"></span>
+   
+    // generate question elements  
+    const questions = quizQuestions.map((e,i) => {
+        return (
+            <SingleQuestion 
+            questions={e} 
+            answerSelected={answerSelected} 
+            key={nanoid()}
+            />
         )
     })
-    console.log(answerElements)
-      return( 
-        <div>
-        <h2 dangerouslySetInnerHTML={{__html: e.question}} />
-        {answerElements}
-        </div>
-        
-
-    )
-  })
 
   return (
     <div>
